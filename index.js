cc222138f774ab961ae723eefd37627f2111451b
Tulio <tulioluv@gmail.com>
2025-03-17 14:38:28 -0300

    fix: translation to english

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-function Calcular(){
-
+function Calculate() {
     var A1 = parseFloat(document.getElementById("A1").value);
     var A2 = parseFloat(document.getElementById("A2").value);
     var A3 = parseFloat(document.getElementById("A3").value);
-    var media = document.getElementById("media")
+    var average = document.getElementById("average");
+    var approval = document.getElementById("approval");
 
-    if(isNaN(A1) || isNaN(A2) || isNaN(A3)){
-        media = media.textContent = "Digite suas médias corretamente."
-    }else if(A1 < 0 || A2 < 0 || A3 < 0){
-        media = media.textContent = "Seu numero é menor que zero."
-    }else if(A1 > 10 || A2 > 10 || A3 > 10){
-        media = media.textContent = "10 é o numero maximo permitido."
-    }else{
-    A1 = A1 * 3
-    A2 = A2 * 3
-    A3 = A3 * 4
+    if (isNaN(A1) || isNaN(A2) || isNaN(A3)) {
+        average.textContent = "Please enter your grades correctly.";
+    } else if (A1 < 0 || A2 < 0 || A3 < 0) {
+        average.textContent = "Your number is less than zero.";
+    } else if (A1 > 10 || A2 > 10 || A3 > 10) {
+        average.textContent = "10 is the maximum allowed number.";
+    } else {
+        A1 = A1 * 3;
+        A2 = A2 * 3;
+        A3 = A3 * 4;
 
-    resultado = (A1 + A2 + A3) / 10
+        var result = (A1 + A2 + A3) / 10;
+        average.textContent = "Your average is " + result.toFixed(1);
 
-    media = media.textContent = "Sua média é " + (resultado.toFixed(1))
+    if (result >= 7){
+        approval.style.color = "rgb(30, 196, 113)";
+        approval.style.fontWeight = "bold";
+        approval.textContent = "You are aproved!"
+    }
+    else{
+        approval.style.color = "rgb(206, 58, 21)";
+        approval.style.fontWeight = "bold";
+        approval.textContent = "You are reproved!"
+    }
+
     }
 }
